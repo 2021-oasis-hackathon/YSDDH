93ef02eccfe752fb20027b0c7b6e28429c9ac010
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-08-15 21:54:53 +0900

    [test] app size

---
+++ OasisTest/OasisTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OasisTest
+//
+//  Created by 최은기 on 2021/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OasisTest/OasisTest/OasisTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OasisTestApp.swift
+//  OasisTest
+//
+//  Created by 최은기 on 2021/08/15.
+//
+
+import SwiftUI
+
+@main
+struct OasisTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
