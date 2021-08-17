c0759d1ae33f581501b89ffe59247d50f8b1a7fe
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-08-17 13:07:56 +0900

    add funtion

--- js/script.js
+++ js/script.js
@@ -12,3 +12,21 @@ $(function(){
   }
 })
 })
+
+var top1 = $('#one').top;
+var top2 = $('#two').top;
+var top3 = $('#three').top;
+var top4 = $('#four').top;
+
+var goto1 = document.getElementById('go1');
+var goto2 = document.getElementById('go2');
+var goto3 = document.getElementById('go3');
+var goto4 = document.getElementById('go4');
+
+function moveScrollTo(par){
+  window.scrollTo({top:par, behavior:'smooth'})
+}
+goto1.addEventListener("click",function(){moveScrollTo(top1)})
+goto2.addEventListener("click",function(){moveScrollTo(top2)})
+goto3.addEventListener("click",function(){moveScrollTo(top3)})
+goto4.addEventListener("click",function(){moveScrollTo(top4)})
