a21f65c08ea1936408a1b4b3c95042d873a337ea
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-08-19 08:48:14 +0900

    map

--- js/pyeong.js
+++ js/pyeong.js
@@ -1,13 +1,15 @@
 //지도 선언
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
-  center: new kakao.maps.LatLng(35.14617,127.60335), //지도의 중심좌표.
+  center: new kakao.maps.LatLng(35.26786,127.64785), //지도의 중심좌표.
   level: 7 //지도의 레벨(확대, 축소 정도)
 };
-var markerPosi = new kakao.maps.LatLng(35.14617,127.60335);
+var markerPosi = new kakao.maps.LatLng(35.26786,127.64785);
   var marker = new kakao.maps.Marker({
     position: markerPosi
   });
   
 var map = new kakao.maps.Map(container, options);
 marker.setMap(map);
+
+
--- js/searoad.js
+++ js/searoad.js
@@ -1,13 +1,16 @@
+
 //지도 선언
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
-  center: new kakao.maps.LatLng(35.14617,127.60335), //지도의 중심좌표.
+  center: new kakao.maps.LatLng(35.33929, 126.38598), //지도의 중심좌표.
   level: 7 //지도의 레벨(확대, 축소 정도)
 };
-var markerPosi = new kakao.maps.LatLng(35.14617,127.60335);
+var markerPosi = new kakao.maps.LatLng(35.33929, 126.38598);
   var marker = new kakao.maps.Marker({
     position: markerPosi
   });
   
 var map = new kakao.maps.Map(container, options);
 marker.setMap(map);
+
+
