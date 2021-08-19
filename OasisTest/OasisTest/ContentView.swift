aa45507d839c99498076034bce2f9df61a0e6550
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-08-19 10:25:58 +0900

    proto end

--- OasisTest/OasisTest/ContentView.swift
+++ OasisTest/OasisTest/ContentView.swift
@@ -6,13 +6,55 @@
 //
 
 import SwiftUI
+//
+//  GallaryView.swift
+//  oasis
+//
+//  Created by 최은기 on 2021/08/17.
+//
+
+
+enum GridType: CaseIterable{
+    case triple
+    var column :[GridItem] {
+        switch self{
+        
+        case .triple:
+            return [GridItem(.flexible()),
+                    GridItem(.flexible()),
+                    GridItem(.flexible())]
+        }
+    }
+    
+}
+
+struct GallaryView: View {
+    
+    var images: [String] = ["starImg","starImg"]
+    var gridType : GridType = .triple
+    
+    var body: some View {
+        ScrollView{
+            LazyVGrid(columns: gridType.column, content: {
+                ForEach(images, id: \.self){ image in
+                    Image("starImg")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+            })
+            
+        }
+        
+        
+    }
+}
+
+
 
 struct ContentView: View {
     var body: some View {
         VStack{
-            Image(uiImage:UIImage(named: "1234") ?? UIImage(named: "text.bubble")!)
-            Spacer()
-            Spacer()
+            GallaryView()
         }
         
 
